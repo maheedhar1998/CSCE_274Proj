96dd630f2973f877335169ba9beac37ad607b41f
Maheedhar Mandapati <maheedhar1998@hotmail.com>
2017-09-27 12:15:19 -0400

    Add files via upload

--- serialInterface.py
+++ serialInterface.py
@@ -1,52 +1,56 @@
+import time
+import struct
 import serial
 class sInterface:
 	def __init__(self):
-		serialC = serial.Serial('/dev/ttyUSB0', 115200)
+		self.serialC = serial.Serial('/dev/ttyUSB0', 115200)
 	def connect (self):
 		self.serialC.close()
+		time.sleep(1)
 		self.serialC.open()
-		return
+		time.sleep(1)
 	def send(self, opCode):
-		serialC.write(chr(opCode))
-		return
-	def send(self, opCodes):
-		x = ""
-		for i in range(0, opCodes.len):
-			x = x+chr(opCodes[i])
-		self.serialC.write(x)
-		return
-	def read(self):
+		self.serialC.write(chr(opCode))
+		time.sleep(0.015)
+	#def sendMult(self, opCodes):
+		#x = ""
+		#for i in range(0, opCodes.len):
+			#x = x+chr(opCodes[i])
+		#self.serialC.write(x)
+	def readD(self):
 		x = self.serialC.inWaiting()
+		#print(x)
 		return self.serialC.read(x)
 	def close(self):
 		self.serialC.close()
-		return
 class rInterface:
-	def __init__():
-		robot = sInterface()
-		robot.connect
-		start = 128
-		reset = 7
-		stop = 173
-		safe = 131
-		sensors = 142
-		buttonPacketID = 18
-		drive = 137
+	def __init__(self):
+		self.robot = sInterface()
+		self.robot.connect()
+		self.start = 128
+		self.reset = 7
+		self.stop = 173
+		self.safe = 131
+		self.sensors = 142
+		self.buttonPacketID = 18
+		self.drive = 137
 	def changeState(self, state):
 		if(str(state) == 'start' or str(state) == 'passive'):
-			self.robot.send(start)
+			self.robot.send(self.start)
 		elif(str(state) == 'reset'):
-			self.robot.send(reset)
+			self.robot.send(self.reset)
 		elif(str(state) == 'stop'):
-			self.robot.send(stop)
+			self.robot.send(self.stop)
 		elif(str(state) == 'safe'):
-			self.robot.send(safe)
+			self.robot.send(self.safe)
 		else:
 			print "Error: Invalid arguement"
+		time.sleep(1)
 	def stateOfButton(self, button):
-		self.robot.send({sensors, buttonPacketID})
-		data = self.robot.read
-		data = int(state)
+		self.robot.send(self.sensors)
+		self.robot.send(self.buttonPacketID)
+		data = self.robot.readD()
+		data = struct.unpack('B', data)[0]
 		if(button.lower() == 'clock'):
 			return bool(data & 0x80)
 		elif(button.lower() == 'schedule'):
@@ -63,11 +67,15 @@ class rInterface:
 			return bool(data & 0x02)
 		elif(button.lower() == 'clean'):
 			return bool(data & 0x01)
-	def drive(self, velocityH, velocityL, radiusH, radiusL):
-		self.robot.send(chr(drive)+chr(velocityH)+chr(velocityL)+chr(radiusH)+chr(radiusL))
-		return
+	def drives(self, velocityH, velocityL, radiusH, radiusL):
+		self.robot.send(self.drive)
+		self.robot.send(velocityH)
+		self.robot.send(velocityL)
+		self.robot.send(radiusH)
+		self.robot.send(radiusL)
 class main:
-	iRobot = sInterface()
-	iRobot.connect()
-	iRobot.send(128)
-
+	iRobot = rInterface()
+	iRobot.changeState('start')
+	iRobot.changeState('safe')
+	iRobot.drives(0,57,0,0)
+	print(iRobot.stateOfButton('clock'))
